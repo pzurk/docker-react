4d911c38ce8f40fb89a9bda80402808c79468db3
Piotr <piotr.zurkowski159@gmail.com>
2025-10-17 17:37:12 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          xddupdated
+          Feature branch change
         </p>
         <a
           className="App-link"
